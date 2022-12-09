5f6171ce8c794c25bcb4ab2493787ff10088871b
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-09 02:30:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,4 @@ io.on("connection", (socket) => {
     }
   });
 });
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
